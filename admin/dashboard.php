ec694387468fa801ba63812ad8e26be4830f7bb0
nahid-mahmud-1 <nahid.mahmud538@gmail.com>
2023-10-27 20:53:35 +0600

    Initialized admin dashboard environment

---
+++ admin/dashboard.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Google Fonts Montserrat & Poppins -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800&family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+        <!-- Reset CSS File -->
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <!-- Grid CSS File -->
+        <link rel="stylesheet" type="text/css" href="css/grid.css">
+        <!-- FontAwesome CSS file -->
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <!-- Theme Main CSS File -->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Admin Dashboard</title>
+    </head>
+    <body>
+        <!-- Theme Option Section Start -->
+        <section class="theme-option-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <!-- Option Box Start -->
+                        <div class="option-box">
+                            <!-- Option Title -->
+                            <div class="option-title">
+                                <h5>Color</h5>
+                            </div>
+                            <!-- Option Form Start -->
+                            <div class="option-form-box">
+                                <form action="" method="POST">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <label for="theme-main-color" class="form-label">Theme Main Color</label>
+                                            <input type="color" name="theme-main-color" id="theme-main-color" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="theme-heading-color" class="form-label">Theme Heading Color</label>
+                                            <input type="color" name="theme-heading-color" id="theme-heading-color" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="theme-text-color-1" class="form-label">Theme Text Color 1</label>
+                                            <input type="color" name="theme-text-color-1" id="theme-text-color-1" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="theme-text-color-2" class="form-label">Theme Text Color 2</label>
+                                            <input type="color" name="theme-text-color-2" id="theme-text-color-2" value="" class="form-control">
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <label for="theme-bg-color" class="form-label">Theme BG Color</label>
+                                            <input type="color" name="theme-bg-color" id="theme-bg-color" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="theme-button-color" class="form-label">Theme Button Color</label>
+                                            <input type="color" name="theme-button-color" id="theme-button-color" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="theme-button-hover" class="form-label">Theme Button Hover</label>
+                                            <input type="color" name="theme-button-hover" id="theme-button-hover" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="theme-border-color-1" class="form-label">Theme Border Color 1</label>
+                                            <input type="color" name="theme-border-color-1" id="theme-border-color-1" value="" class="form-control">
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <label for="theme-item-box-color" class="form-label">Theme Item Box Color</label>
+                                            <input type="color" name="theme-item-box-color" id="theme-item-box-color" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="theme-item-box-hover" class="form-label">Theme Item Box Hover</label>
+                                            <input type="color" name="theme-item-box-hover" id="theme-item-box-hover" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="contact-form-button-color" class="form-label">Contact Form Button Color</label>
+                                            <input type="color" name="contact-form-button-color" id="contact-form-button-color" value="" class="form-control">
+                                        </div>
+
+                                        <div class="col-3">
+                                            <label for="contact-form-button-hover" class="form-label">Contact Form Button Hover</label>
+                                            <input type="color" name="contact-form-button-hover" id="contact-form-button-hover" value="" class="form-control">
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-3">
+                                                <label for="theme-white" class="form-label">Theme White</label>
+                                                <input type="color" name="theme-white" id="theme-white" value="" class="form-control">
+                                            </div>
+
+                                            <div class="col-3">
+                                                <label for="theme-black" class="form-label">Theme Black</label>
+                                                <input type="color" name="theme-black" id="theme-black" value="" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Submit Button -->
+                                    <div class="row">
+                                        <input type="submit" name="option-submit-button" id="option-submit-button" value="Save" class="option-submit-button">
+                                    </div>
+                                </form>
+                            </div>
+                            <!-- Option Form End -->
+                        </div>
+                        <!-- Option Box End -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Theme Option Section End -->
+
+
+
+
+
+
+
+        <!-- Theme JS File -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
